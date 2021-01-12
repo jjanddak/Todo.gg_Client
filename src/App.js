c4193a61eeb51e2de4d213a1a3b0389686517eb1
Hyunwoo Han <h900506@gmail.com>
2021-01-12 16:55:12 +0900

    feature/signup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  withRouter,
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Signup from "./component/Signup";
+// import Login from './component/Login';
+
+class App extends React.Component{
+  render(){
+    return(
+      <>
+        <h1>hello world</h1>
+        <Router>
+          <Switch>
+            <Route path="/user/signup">
+              <Signup />
+            </Route>
+            {/* <Route patn="/user/login">
+              <Login />
+            </Route> */}
+          </Switch>
+        </Router>
+      </>
+    )
+  }
 }
 
-export default App;
+export default withRouter(App);
---
+++ src/component/Signup.js
@@ -0,0 +1,164 @@
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import axios from "axios";
+axios.defaults.withCredentials = true;
+
+function Signup() {
+  const history = useHistory();
+  let emailChecked = false;
+  const [email, setEmail] = useState("");
+  const [emailMessage, setEmailMessage] = useState("");
+  let usernameChecked = false;
+  const [username, setUsername] = useState("");
+  const [usernameMessage, setUsernameMessage] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstPassword, setFirstPassword] = useState("");
+  const [lastPassword, setLastPassword] = useState("");
+  const [passwordMessage, setPasswordMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const getKeyword = (e) => {
+    if (e.target.name === "email") setEmail(e.target.value);
+    if (e.target.name === "username") setUsername(e.target.value);
+    if (e.target.name === "firstPassword") {
+      setFirstPassword(e.target.value);
+      checkPassword(e.target.value, lastPassword);
+    }
+    if (e.target.name === "lastPassword") {
+      setLastPassword(e.target.value);
+      checkPassword(firstPassword, e.target.value);
+    }
+  };
+  const checkEmail = () => {
+    function isEmail(asValue) {
+      let regExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+      return regExp.test(asValue);
+    }
+    if (!email) {
+      setEmailMessage("이메일을 입력 해 주세요");
+      emailChecked = false;
+    } else if (!isEmail(email)) {
+      setEmailMessage("이메일 형식이 올바르지 않습니다");
+      emailChecked = false;
+    } else {
+      axios.post("https://localhost:4001/user/checkEmail", {
+        email: email
+      })
+        .then(res => {
+          if (res.data) {
+            setEmailMessage("이미 가입된 이메일 입니다");
+            emailChecked = false;
+          } else {
+            setEmailMessage("사용 가능한 이메일 입니다");
+            emailChecked = true;
+          }
+        })
+    }
+  };
+  const checkUsername = () => {
+    if (username === "") {
+      setUsernameMessage("닉네임을 입력 해 주세요");
+      usernameChecked = false;
+    } else {
+      axios.post("https://localhost:4001/user/checkUsername", {
+        username: username
+      })
+        .then(res => {
+          if (res.data.message === "invalid") {
+            setUsernameMessage("사용 중인 닉네임 입니다");
+            usernameChecked = false;
+          } else {
+            setUsernameMessage("사용 가능한 닉네임 입니다");
+            usernameChecked = true;
+          }
+        })
+    }
+  };
+  const checkPassword = (pw1, pw2) => {
+    if (pw1.length < 1 || pw2.length < 1) {
+      setPasswordMessage("비밀번호를 입력 해 주세요");
+    } else if (pw1.length < 6 || pw1.length > 12) {
+      setPasswordMessage("비밀번호는 6~12자리 이내로 입력 해 주세요");
+    } else if (pw1 === pw2) {
+      setPasswordMessage("비밀번호가 일치합니다");
+      setPassword(pw1);
+    } else {
+      setPasswordMessage("비밀번호가 일치하지 않습니다");
+    }
+  };
+  const handleSignup = () => {
+    if (!username || !password || !email) {
+      setErrorMessage("모든 항목은 필수입니다");
+    } else if (!emailChecked || !usernameChecked) {
+      setErrorMessage("이메일과 닉네임 중복 확인을 해주세요");
+    } else {
+      axios.post("https://localhost:4001/user/signup", {
+        username: username,
+        password: password,
+        email: email
+      })
+        .then(() => {
+          history.push("/user/login");
+        });
+    }
+  };
+
+  return (
+    <div className="Signup">
+      <h2 className="Signup_title">SignUp</h2>
+      <span className="Signup_subtitle">이메일</span>
+      <button
+        className="SignUp_check_button"
+        onClick={checkEmail}
+      >중복확인</button>
+      <input
+        className="Signup_input"
+        name="email"
+        type="text"
+        onChange={getKeyword}
+      />
+      <div className="Signup_alert_box">{emailMessage}</div>
+      <span className="Signup_subtitle">비밀번호</span>
+      <input
+        className="Signup_input"
+        name="firstPassword"
+        type="password"
+        onChange={getKeyword}
+        value={firstPassword}
+      />
+      <span className="Signup_subtitle">비밀번호 확인</span>
+      <input
+        className="Signup_input"
+        name="lastPassword"
+        type="password"
+        onChange={getKeyword}
+        value={lastPassword}
+      />
+      <div className="Signup_alert_box">{passwordMessage}</div>
+      <span className="Signup_subtitle">닉네임</span>
+      <button
+        className="SignUp_check_button"
+        type="submit"
+        onClick={checkUsername}
+      >중복확인</button>
+      <input
+        className="Signup_input"
+        name="username"
+        type="text"
+        onChange={getKeyword}
+      />
+      <div className="Signup_alert_box">{usernameMessage}</div>
+      <div>
+        <Link to="/user/login">이미 아이디가 있으신가요?</Link>
+      </div>
+      <div className="Signup_alert_box">{errorMessage}</div>
+      <button
+        className="SignUp_submit_button"
+        onClick={handleSignup}
+      >
+        회원가입
+        </button>
+    </div>
+  )
+}
+
+export default Signup;
