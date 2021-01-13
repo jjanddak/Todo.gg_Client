c74dec2cf3036b8f8e6553f3912a0563e28b14fe
Hyunwoo Han <h900506@gmail.com>
2021-01-13 10:51:15 +0900

    feature/#12/signup with profile

--- src/App.js
+++ src/App.js
@@ -5,31 +5,28 @@ import {
   Route,
   withRouter,
 } from "react-router-dom";
+
 import Login from './component/Login';
 import Signup from "./component/Signup";
-class App extends React.Component{
-  render(){
-    return(
+
+class App extends React.Component {
+  render() {
+    return (
       <>
         <h1>hello world</h1>
         <Router>
           <Switch>
-            {/* <Route path="/user/signup">
-              <Signup />
-            </Route> */}
             <Route path="/user/login">
               <Login />
             </Route>
             <Route path="/user/signup">
               <Signup />
             </Route>
-            {/* <Route patn="/user/login">
-              <Login />
-            </Route> */}
           </Switch>
         </Router>
       </>
     )
   }
 }
+
 export default withRouter(App);
