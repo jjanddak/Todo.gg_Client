3d8ef24bf5a37fc8c819462b62686a32e26c286e
chaerang2 <codbs54@gmail.com>
2021-01-13 11:00:41 +0900

    #10/fakedata생성

---
+++ src/component/fakeproject.js
@@ -0,0 +1,26 @@
+export const project ={
+    projectList:[
+        {
+            title:'프로젝트1이름',
+            date:'2021.01.15~2021.02.10',
+            description:' 프로젝트는 어찌구저찌구 이렇게 하는겁니다',
+            member:
+            [
+                { profile:'../img/레킹볼.png' },
+                { profile:'../img/레킹볼.png' },
+                { profile:'../img/레킹볼.png' }
+            ]
+        },
+        {
+            title:'이렇게 쓰시면 됩니다',
+            date:'2021.01.15~2021.02.10',
+            description:'레킹볼은 생각보다 귀엽습니다',
+            member:
+            [
+                { profile:'../img/레킹볼.png' },
+                { profile:'../img/레킹볼.png' },
+                { profile:'../img/레킹볼.png' }
+            ]
+        }
+    ]
+}
