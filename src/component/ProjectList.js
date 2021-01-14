a3fc42961beb3c1ae656dbec7865378cb92ed523
chaerang2 <codbs54@gmail.com>
2021-01-14 23:17:27 +0900

    #7/모달창 구현 및 kda 퍼센티지 적용

--- src/component/ProjectList.js
+++ src/component/ProjectList.js
@@ -1,15 +1,16 @@
 import React, { useState } from "react";
 import ProjectListEntry from './ProjectListEntry'
 import { Link } from "react-router-dom";
-import fakeproject from './fakeproject.js'
-import Login from './Login'
-import Signup from './Signup'
+import fakeproject from './fakeproject.js';
+import Login from './Login';
+import Signup from './Signup';
+import axios from 'axios';
 
 
 function ProjectList() {
   const [state, setState] = useState({ //모달창을위한 state
     login: false,
-    signup: false
+    signup: false,
   })
   const { login, signup } = state;
   const loginChange = () => {
@@ -18,20 +19,45 @@ function ProjectList() {
   const signupChange = () => {
     setState({ ...state, signup: !signup, login: false })
   }
-
+  const taskCardCount = fakeproject.taskCardCount
   const { isLogin } = window.sessionStorage //네비와 메인을 위한 세션읽기
-  const list = isLogin
-    ? fakeproject.List.map(ele => {
-      return <ProjectListEntry key={ele.id} content={ele}></ProjectListEntry>
+  let list = isLogin //로그인 상태별 리스트
+    ? fakeproject.contributers.map((ele, idx) => {
+      return <ProjectListEntry key={idx} content={ele} taskCardCount={taskCardCount[idx]}></ProjectListEntry>
     })
-    : fakeproject.List.map(ele => {
-      return <ProjectListEntry key={ele.id} content={ele}></ProjectListEntry>
+    : fakeproject.contributers.map((ele, idx) => {
+      return <ProjectListEntry key={idx} content={ele} taskCardCount={taskCardCount[idx]}></ProjectListEntry>
     })
+
+  const test = function () {
+    axios.post('https://localhost:4001/', null)
+      .then(param => {
+        param.data.contributers.map((ele, idx) => {
+          return <ProjectListEntry key={idx} content={ele} taskCardCount={taskCardCount[idx]}></ProjectListEntry>
+        })
+      })
+  }
+  console.log(test())
+  let done = 0 // kda계산
+  let inprogress = 0
+  let todo = 0
+  fakeproject.taskCardCount.map(ele => {
+    done = done + ele.done;
+    inprogress = inprogress + ele.inprogress;
+    todo = todo + ele.todo;
+  })
 
   const main = isLogin
-  ?<h1>업무티어 올리기 투두관리부터 !<br />협업은 todo.gg와 함께</h1>
-  :<h1>업무티어 올리기 투두관리부터 !<br />협업은 todo.gg와 함께</h1>
-
+    ? <div>
+      <img src={fakeproject.profile}></img>
+      <p>{fakeproject.username}</p>
+      <p>todo:{todo}</p>
+      <p>inprogress:{inprogress}</p>
+      <p>done:{done}</p>
+      <button>프로필수정</button>
+    </div>
+    : <h1>업무티어 올리기 투두관리부터 !<br />협업은 todo.gg와 함께</h1>
+    
 
 
   return (
@@ -39,7 +65,7 @@ function ProjectList() {
       <nav>
         <Link to='/'>로고</Link>
         {
-          isLogin && <Link>로그아웃</Link>
+          isLogin && <button>로그아웃</button>
         }
       </nav>
       {main}
@@ -47,6 +73,7 @@ function ProjectList() {
       <button onClick={signupChange}>회원가입</button>
       { login && <Login loginChange={loginChange} signupChange={signupChange} />}
       { signup && <Signup loginChange={loginChange} signupChange={signupChange} />}
+      <button>+</button>
       {list}
     </>
   )
--- src/component/ProjectListEntry.js
+++ src/component/ProjectListEntry.js
@@ -1,27 +1,44 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 import './css/ProjectListEntry.css'
 
-function ProjectListEntry({content}) {
-  const teamList = content.member.map(ele=>{
-    return <img className='entry_teamimg'key={ele.id}src={ele.profile}></img>
+function ProjectListEntry({content,taskCardCount}) {
+  const {project} = content
+  const startDate = project.start_date.slice(0,10)
+  const endDate = project.end_date.slice(0,10)
+
+  const { done, inprogress, todo} = taskCardCount
+  const sum = Math.round(done/(inprogress+todo+done) *100) // 합계구하기
+
+  let color //state 색상변경
+  if(sum <= 75){
+    color = {backgroundColor : 'red'}
+  }else if(sum > 76 && sum <=99){
+    color = {backgroundColor : 'yellow'}
+  }else{
+    color = {backgroundColor : 'blue'}
+  }
+  const teamList = content.project.contributers.map(ele=>{ //팀원프사
+    return <img className='entry_teamimg'key={ele.user_id}src={ele.user.profile}></img>
   })
+
   return (
-    <div className='entry'>
-      <div className='entry_stateColor'></div>
+    <Link to={`/project/${content.project_id}`} className='entry' >
+      <div className='entry_stateColor' style={color}></div>
       <div className='box'>
-        <p className='entry_title'>{content.title}<br />{content.date}</p>
+        <p className='entry_title'>{project.title}<br />{startDate}<br />{`~${endDate}`}</p>
         {/* <p className='entry_date'>{content.date}</p> */}
       </div>
-      <img src={content.hostImg} className='entry_host'></img>
+      <img src={project.user.profile} className='entry_host'></img>
       <div className='box'>
-        <p className='entry_kda'>10/5/13</p>
-        <p className='entry_progress'>진행 20%</p>
+        <p className='entry_kda'>{`${todo}/${inprogress}/${done}`}</p>
+        <p className='entry_progress'>{`진행 ${sum}%`}</p>
       </div>
-      <p className='entry_description'>{content.description}</p>
+      <p className='entry_description'>{project.description}</p>
       <div className='entry_team'>
         {teamList}
       </div>
-    </div> 
+    </Link> 
   )
 }
 
