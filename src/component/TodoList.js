051071557f479cc46042a2fd48a42f6178abe680
Hyunwoo Han <h900506@gmail.com>
2021-01-19 16:49:07 +0900

    feature/#9/logout+addcard+list_boxing

--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -3,6 +3,7 @@ import { useHistory } from "react-router-dom";
 import axios from "axios";
 
 import dummy from "./faketodo";
+import "./css/todoList.css";
 
 axios.defaults.withCredentials = true;
 
@@ -11,36 +12,36 @@ function TodoList() {
   const projectId = window.location.href.split("/")[4]; //* url 에서 아이디 가져오기
   const isLogin = window.sessionStorage.isLogin;
 
-  const [project, setProject] = useState({
-    id: 0,
-    title: "",
-    description: "",
-    start_date: "",
-    end_date: "",
-    contributers: [],
-    taskCards: [],
+  const [project, setProject] = useState({ //* 프로젝트 초기화
+    id: 0, title: "", description: "", start_date: "", end_date: "", contributers: [], taskCards: [],
   });
-  const [counts, setCount] = useState({
-    todo: 0,
-    inprogress: 0,
-    done: 0,
-  });
-
+  const [counts, setCount] = useState({ todo: 0, inprogress: 0, done: 0, }); //* 카운트 초기화
+  const [taskContent, setTaskContent] = useState("");
+  const [showAddCard, setShowAddCard] = useState(false);
   const process = Math.round(counts.done / (counts.todo + counts.inprogress + counts.done) * 100);
   const todoList = [];
   const inprogressList = [];
   const doneList = [];
   project.taskCards.forEach((item) => {
-    item.state === "todo" && todoList.push(<div className="todoList_todo_entry" key={item.id}>{item.content}</div>)
-    item.state === "inprogress" && inprogressList.push(<div className="todoList_inprogress_entry" key={item.id}>{item.content}</div>)
-    item.state === "done" && doneList.push(<div className="todoList_done_entry" key={item.id}>{item.content}</div>)
+    item.state === "todo" && todoList.push(<div className="todoList_todo_entry" key={item.id}>
+      {item.content}
+      <button className="todoList_delete_card">delete card</button>
+    </div>)
+    item.state === "inprogress" && inprogressList.push(<div className="todoList_inprogress_entry" key={item.id}>
+      {item.content}
+      <button className="todoList_delete_card">delete card</button>
+    </div>)
+    item.state === "done" && doneList.push(<div className="todoList_done_entry" key={item.id}>
+      {item.content}
+      <button className="todoList_delete_card">delete card</button>
+    </div>)
   });
   /**
    * DONE 태스크카드 받아서 뿌려주는 것 까지 R
    * TODO 태스크카드 C UD, 햄버거 모달, 진행도 따라 색 표시
    * DONE 3 이상의 아이디 넘어올 때 에러(더미에 2까지 밖에 없어서) -> 하드코딩...
    */
-  useEffect(() => {
+  const getProject = () => {
     if (!isLogin) { //* 로그인 상태 아닐 때
       try {
         setProject(dummy[projectId].projectInfo);
@@ -66,6 +67,9 @@ function TodoList() {
           history.push("/"); //* 에러나면 홈으로
         })
     }
+  }
+  useEffect(() => {
+    getProject();
   }, []);
   let color = { backgroundColor: 'red' }; //* process_color
   if (process > 76 && process <= 99) {
@@ -73,21 +77,71 @@ function TodoList() {
   } else if (process === 100) {
     color = { backgroundColor: 'blue' };
   }
-
+  const onChange = (e) => {
+    setTaskContent(
+      e.target.value,
+    )
+  };
+  const logout = () => {
+    axios.post("https://localhost:4001/user/logout", null, {
+      headers: {
+        Authorization: `Bearer ${window.sessionStorage.accessToken}`,
+        "content-type": "application/json"
+      }
+    })
+      .then(() => {
+        window.sessionStorage.clear();
+        history.push("/");
+      });
+  };
+  const addCard = () => {
+    axios.post(`https://localhost:4001/project/${projectId}/newTask`, {
+      content: taskContent,
+    }, {
+      headers: {
+        Authorization: `Bearer ${window.sessionStorage.accessToken}`,
+        "Content-Type": "application/json"
+      }
+    })
+    .then(() => {
+      setTaskContent("");
+      setShowAddCard(false);
+      getProject();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  };
   return (
     <div className="todoList">
       <nav className="todoList_nav">
-        <div className="todoList_precess_color" style={color} />
-        <div className="todoList_title">{project.title}</div>
-        <div className="todoList_date">{`${project.start_date} ~ ${project.end_date}`}</div>
-        <div className="todoList_process">{`진행도 ${process}%`}</div>
-        <button className="todoList_logout"></button>
-        <button className="todoList_home" onClick={() => { history.push("/") }}></button>
-        <button className="todoList_set_project"></button>
+        <div className="todoList_process_color" style={color} />
+        <span className="todoList_title">{project.title}</span>
+        <span className="todoList_date">{`${project.start_date} ~ ${project.end_date}`}</span>
+        <span className="todoList_process">{`진행도 ${process ? process : 0}%`}</span>
+        {isLogin && <button className="todoList_logout" onClick={logout}>로그아웃</button>}
+        <button className="todoList_home" onClick={() => { history.push("/") }}>홈버튼</button>
+        <button className="todoList_set_project">프로젝트 관리</button>
       </nav>
-      todo<div className="todoList_todo">{todoList}</div>
-      inprogress<div className="todoList_inprogress">{inprogressList}</div>
-      done<div className="todoList_done">{doneList}</div>
+      <div className="todoList_taskCards">
+        <div className="todoList_todo">todo
+          <button className="todoList_add_card" onClick={() => setShowAddCard(!showAddCard)}>add card</button>
+          <div className="todoList_counts">{counts.todo}</div>
+          <div className="todoList_add_" style={{display:showAddCard ? "block" : "none"}}>
+            <input type="text" className="todoList_input" onChange={onChange} value={taskContent} />
+            <button className="todoList_submit_input" onClick={addCard}>add</button>
+          </div>
+          {todoList}
+        </div>
+        <div className="todoList_inprogress">inprogress
+          <div className="todoList_counts">{counts.inprogress}</div>
+          {inprogressList}
+        </div>
+        <div className="todoList_done">done
+          <div className="todoList_counts">{counts.done}</div>
+          {doneList}
+        </div>
+      </div>
     </div>
   )
 }
