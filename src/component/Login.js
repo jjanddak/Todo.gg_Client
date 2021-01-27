1837c7f4279c6b21adb15bf18b3ae08f2677e581
jjanddak <opplane@gmail.com>
2021-01-27 10:34:24 +0900

    App.js gitSocial edit

--- src/component/Login.js
+++ src/component/Login.js
@@ -60,52 +60,7 @@ class Login extends React.Component {
     console.log(err);
   }
 
-  //github Login
-  getAccessToken = async (authorizationCode) => {
-    // 받아온 authorization code로 다시 OAuth App에 요청해서 access token을 받을 수 있습니다.
-    // access token은 보안 유지가 필요하기 때문에 클라이언트에서 직접 OAuth App에 요청을 하는 방법은 보안에 취약할 수 있습니다.
-    // authorization code를 서버로 보내주고 서버에서 access token 요청을 하는 것이 적절합니다.
-    // TODO: 서버의 /callback 엔드포인트로 authorization code를 보내주고 access token을 받아옵니다.
-    await axios.post('https://localhost:4001/user/callback',
-    {
-      authorizationCode: authorizationCode
-    })
-    .then((res=>{
-      const accessToken=res.data.accessToken;
-      axios.get('https://api.github.com/user',
-      {
-        headers:{
-          authorization: `token ${accessToken}`
-        },
-        withCredentials:false
-      })
-      .then(res=>{
-        axios.post("https://localhost:4001/user/githubLogin",{
-          data:res.data
-        })
-        .then(result=>{
-          window.sessionStorage.clear()
-          window.sessionStorage.username=result.data.userinfo.username;
-          window.sessionStorage.profile=result.data.userinfo.profile;
-          window.sessionStorage.email=result.data.userinfo.email;
-          window.sessionStorage.accessToken=result.data.accessToken;
-          window.sessionStorage.id = result.data.userinfo.id;
-          window.sessionStorage.isLogin=true;
-          this.setState({gitLogin:!this.state.gitLogin});
-          this.props.history.push("/");
-        })
-      })
-    }))
-  }
-  componentDidMount() {
-    const url = new URL(window.location.href)
-    const authorizationCode = url.searchParams.get('code')
-    if (authorizationCode) {
-      // authorization server로부터 클라이언트로 리디렉션된 경우, authorization code가 함께 전달됩니다.
-      // ex) http://localhost:3000/?code=5e52fb85d6a1ed46a51f
-      this.getAccessToken(authorizationCode)
-    }
-  }
+  
 
   socialLoginHandler() { 
     window.location.assign(this.GITHUB_LOGIN_URL)
@@ -148,7 +103,7 @@ class Login extends React.Component {
           <input name='inputPassword' type='password' onChange={this.handleInputValue('password')} autoComplete='off' required></input>
           <label for='inputPassword'><span>Password</span></label>
           </p>
-          <div>{this.state.errorMessage}</div>
+          <div className='Signup_alert_box'>{this.state.errorMessage}</div>
           <div className='btnwrapper'>
             <button className='modalbtn' onClick={this.handleLogin}>
               로그인
